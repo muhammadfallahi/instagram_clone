3c3ad5f2ff412e38ed06466951dbec81e3e826f7
muhammad <muhammadfallahi6@gmail.com>
2021-10-20 19:39:34 +0330

    add middleware to api auth and refact login method

--- app/Http/Controllers/ApiController/ApiAuthController.php
+++ app/Http/Controllers/ApiController/ApiAuthController.php
@@ -8,21 +8,20 @@ use Illuminate\Support\Facades\Auth;
 
 class ApiAuthController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+    
     public function login(loginRequest $request)
     {
 
-        if (!$token = auth('api')->attempt($request->validated())) {
+        if (! $token = auth('api')->attempt($request->validated())) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
         
 
-        return response()
-        ->json([
-            'message' => 'login successfully',
-            'data' => [
-                'token' => $token
-            ]
-        ]);
+        return $this->createNewToken($token);
 
     }
 
@@ -35,5 +34,15 @@ class ApiAuthController extends Controller
     public function me()
     {
         return auth('api')->user();
+    }
+
+
+    protected function createNewToken($token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'user' => auth('api')->user()
+        ]);
     }
 }
